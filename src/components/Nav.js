e44b9f3a6d1bf87d78714a3eb1b3afcf8a4d6112
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-05-23 17:34:45 +0200

    Create empty view and use NavLink in Nav comp.

---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+import logo from '../images/planet.png';
+
+export default function Nav() {
+  const routLinks = [
+    {
+      path: '/',
+      text: 'View',
+    },
+  ];
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="left-nav">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="right-nav">
+          <ul className="nav-links">
+            {routLinks.map((rout) => (
+              <li key={uuid()} className="nav-link">
+                <NavLink to={rout.path}>{rout.text}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
