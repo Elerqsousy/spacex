297c8f66dcfbf33e312035bfbf05a35ab5142a97
ekenecf <ekens2u@gmail.com>
2022-05-23 15:37:50 +0100

    Create project header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './navigation';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <Nav />
       </header>
     </div>
   );
